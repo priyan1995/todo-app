829a7fd1bc1ab97f55d365fca439532fb1b490b8
priyan <priyan.abewickrama@ideahub.lk>
2022-06-15 20:26:44 +0530

    rename project

--- src/component/ProjectRename.jsx
+++ src/component/ProjectRename.jsx
@@ -1,14 +1,55 @@
 import React, { useState } from 'react';
+import db from '../services/todoFirebaseService';
 import { ProjectForm } from './ProjectForm';
 
-export const ProjectRename = (props) => {
+export const ProjectRename = ({project,setShowModal }) => {
 
-    const project = props.project;
 
     const [newProjectName, setNewProjectName] = useState(project.name);
 
+    const renameProject = (project,newProjectName) => {
+
+        const projectRef = db.collection('projects');
+        const todosRef = db.collection('todos');
+
+        const { name: oldProjectName } = project;
+
+        projectRef
+        .where('name','==',newProjectName)
+        .get()
+        .then( querySnapshot=>{
+            if(!querySnapshot.empty){
+                alert('Project with the same name already exist!');
+            }else{
+                projectRef
+                .doc(project.id)
+                .update({
+                    name:newProjectName
+                })
+                .then( ()=>{
+                    todosRef
+                    .where('projectName','==',oldProjectName)
+                    .get()
+                    .then( querySnapshot => {
+                        querySnapshot.forEach( doc =>{
+                            doc.ref.update({
+                                projectName: newProjectName
+                            })
+                        })
+                    } )
+                } )
+            }
+        } )
+
+    }
+
     const handleSubmit = (e) => {
         e.preventDefault();
+
+        renameProject(project,newProjectName);
+
+        setShowModal(false);
+
     }
 
     return (
@@ -19,7 +60,7 @@ export const ProjectRename = (props) => {
                     heading="Edit Project!"
                     value={newProjectName}
                     setValue={setNewProjectName}
-                    setShowModal={props.setShowModal}
+                    setShowModal={setShowModal}
                     confirmButtonText="Confirm"
                 />
             </div>
