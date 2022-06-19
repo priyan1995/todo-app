aca992e1ee3b850574de2f12458bf1b70cc940a8
priyan <priyan.abewickrama@ideahub.lk>
2022-06-19 19:14:18 +0530

    hide edit panel on sidebar click

--- src/component/Sidebar.jsx
+++ src/component/Sidebar.jsx
@@ -1,9 +1,35 @@
 import React from 'react';
+import { useEffect } from 'react';
+import { useRef } from 'react';
+import { useContext } from 'react';
+import { TodoContext } from '../context/TodoContext';
 
 export const Sidebar = (props) => {
-    return(
+
+    const { setSelectedTodo } = useContext(TodoContext);
+
+    const sidebarRef = useRef();
+
+    useEffect(()=>{
+
+       document.addEventListener('click', handleClick);
+       
+       return () =>  document.removeEventListener('click', handleClick);
+       
+    })
+
+    const handleClick = (e) => {
+        if(e.target === sidebarRef.current || sidebarRef.current.contains(e.target)){
+            setSelectedTodo(undefined)
+        }
+    }
+
+    return (
         <>
-            <div className='pd-sidebar'>
+            <div
+                className='pd-sidebar'
+                ref={sidebarRef}
+            >
                 {props.children}
             </div>
         </>
